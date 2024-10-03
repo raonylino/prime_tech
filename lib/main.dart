bc1ed0c5011212cdb99b3b07282e2d0f1c72c809
raony lino <raony_lino@hotmail.com>
2024-10-03 17:54:40 -0300

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:prime_tech/firebase_options.dart';
 import 'package:prime_tech/src/prime_tech_app.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const PrimeTechApp());
 }
