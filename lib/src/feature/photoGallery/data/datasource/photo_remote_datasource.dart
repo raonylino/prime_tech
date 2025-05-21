884e9d1c3b5cd5ffa8ae02e997eded8380f04b70
RaonyLino <raony_lino@hotmail.com>
2025-05-21 15:59:09 -0300

    erro ao subir imagem no s3 sim simples3 nao funcuinou

--- lib/src/feature/photoGallery/data/datasource/photo_remote_datasource.dart
+++ lib/src/feature/photoGallery/data/datasource/photo_remote_datasource.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:aws_s3_upload_lite/aws_s3_upload_lite.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:prime_pronta_resposta/src/core/endpoints/app_endpoints.dart';
-import 'package:prime_pronta_resposta/src/core/enums/aws_regions_enum.dart';
 import 'package:prime_pronta_resposta/src/feature/auth/domain/entities/s3_entity.dart';
 import 'package:prime_pronta_resposta/src/feature/photoGallery/domain/entities/photo_upload_entity.dart';
-import 'package:simple_s3/simple_s3.dart';
 
 abstract class PhotoRemoteDatasource {
   Future<void> uploadPhoto(String base64Image, S3Entity s3);
@@ -19,25 +18,27 @@ class PhotoRemoteDatasourceImpl implements PhotoRemoteDatasource {
 
   @override
   Future<void> uploadPhoto(String base64Image, S3Entity s3) async {
-    final SimpleS3 simpleS3 = SimpleS3();
-
-    final imageBytes = utf8.encode(base64Image);
+    final imageBytes = base64Decode(base64Image);
     final fileName = photoUploadEntity.getFileName(imageBytes);
 
-    final url = AppEndpoints.photoUrl(s3: s3);
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/$fileName.jpg');
+    // Cria arquivo temporário
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$fileName.jpg');
     await file.writeAsBytes(imageBytes);
-    final filePath = '$url/service_evidence';
 
-    await simpleS3.uploadFile(
-      file,
-      s3.bucket,
-      'us-east-1:936d86b4-f2e7-4072-a2f4-80c91ec77bba',
-      AWSRegions.usEast1,
-      fileName: fileName,
-      s3FolderPath: filePath,
-      useTimeStamp: false,
-    );
+    try {
+      await AwsS3.uploadFile(
+        accessKey: s3.accessKeyId,
+        secretKey: s3.secretAccessKey,
+        region: s3.region,
+        bucket: s3.bucket,
+        file: file,
+        filename: fileName, // Add this line
+        destDir: s3.directory, // exemplo: 'service_evidence'
+      );
+    } catch (e) {
+      print('Erro ao fazer upload: $e');
+      throw Exception('Erro no upload: $e');
+    }
   }
 }
